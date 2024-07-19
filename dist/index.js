7d62d4509becf3c07cc8db5a227e788b009d94dc
Soham2912 <sohamlahoti@gmail.com>
2024-07-20 00:09:00 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const a = 24;
+//alert(a)
+let b = 50;
+let c = "60";
+//b="hh";
+c = "jj";
+let check;
+
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+const a = 24 ;
+let h;
+let b:number= 50 ;
+
+let c:string ="60";
+//b="hh";
+c = "jj";
+
+let check:boolean;
+
+let l =<string>`lol`;
+
+let version: string | number;
+
+
+const func = (n:string,m:string):string =>{
+    return String(n+m);
+}
+
+const func2 = (n:string,m:string):string =>String(n+m);
